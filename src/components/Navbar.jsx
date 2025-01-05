f26a214c366db8ea1ddbdcf371f175689223016d
Mousa <mhdmousa220@gmail.com>
2025-01-05 21:22:42 +0400

    hoping it will work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -82,7 +82,7 @@ else{
 >
 <nav className='flex size-full items-center justify-between p-4'>
 <div className='flex items-center gap-7'>
-<img src="img/logo.png" alt="logo" className='w-10'/>
+<img src="img/logo.png" alt="lolo" className='w-10'/>
 <CustomButton id="product-button" title="Product" rightIcon={<TiLocationArrow/>}
 containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"/>
 
