11b7d3318263c6b36957c2e6f2bb55111d167c44
Mousa <mhdmousa220@gmail.com>
2025-01-06 15:12:52 +0400

    base changing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/  
 export default defineConfig({
   plugins: [react()],
- // base: '/Awaard-wining/', // Set this to your repository name  
+  base: '/Awaard-wining/', // Set this to your repository name  
 })
